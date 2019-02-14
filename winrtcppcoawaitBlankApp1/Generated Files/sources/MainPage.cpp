bf12311162027389dce0b0e5fb85368757330390
yinyue200 <yinyue200@outlook.com>
2019-02-14 21:58:16 +0800

    Add project files.

---
+++ winrtcppcoawaitBlankApp1/Generated Files/sources/MainPage.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+
+namespace winrt::winrtcppcoawaitBlankApp1::implementation
+{
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t value)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ winrtcppcoawaitBlankApp1/Generated Files/sources/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::winrtcppcoawaitBlankApp1::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::winrtcppcoawaitBlankApp1::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
